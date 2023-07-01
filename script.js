c78b8e4baa955a7a3fe1eb213116ad2fb4e27750
jks6404 <jannusingh.75@gmail.com>
2023-07-01 23:34:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const login=document.getElementById('login-btn');
+const signup= document.getElementById('signup-btn');
+
+login.addEventListener('click',()=>{
+    location.href='./login/index.html';
+})
+
+signup.addEventListener('click',()=>{
+    location.href='./signup/index.html'
+})
